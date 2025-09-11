4cc538b0c93abc1b21faa2541caad13d7cc6ae70
tortuga241 <shopotandrey39@gmail.com>
2025-09-11 22:43:13 +0500

    Patch 1.0

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,36 @@
-console.log('hello');
+console.log("Hello world!");
+
+const butAudio = document.getElementById('butSound');
+const audio = new Audio('./audio/engine.mp3');
+
+audio.loop = true;
+
+function playSound() {
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function stopSound() {
+    audio.pause();
+    audio.currentTime = 0;
+}
+
+butAudio.addEventListener("mousedown", playSound);
+butAudio.addEventListener("mouseup", stopSound);
+
+butAudio.addEventListener("touchstart", playSound);
+butAudio.addEventListener("touchend", stopSound);
+
+
+
+//Плавный скролл для анимации
+let scrolled = false; 
+
+window.addEventListener("scroll", () => {
+  if (!scrolled && window.scrollY > 5) {
+    scrolled = true;
+    document.getElementById("mainPart").scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+});
