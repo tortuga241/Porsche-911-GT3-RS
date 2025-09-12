a2fc9967248bb30b5329fa48ea3977f41472104e
tortuga241 <shopotandrey39@gmail.com>
2025-09-12 21:48:44 +0500

    Patch 2.0

--- JS/script.js
+++ JS/script.js
@@ -34,3 +34,42 @@ window.addEventListener("scroll", () => {
     });
   }
 });
+
+//Анимация цифр (второй блок)
+function animateCounter(el, duration = 700) {
+  const target = parseFloat(el.getAttribute("data-target"));
+  let startTime = null;
+
+  function easeOutCubic(t) {
+    return 1 - Math.pow(1 - t, 3);
+  }
+
+  function updateCounter(currentTime) {
+    if (!startTime) startTime = currentTime;
+    const progress = Math.min((currentTime - startTime) / duration, 1);
+    const easedProgress = easeOutCubic(progress);
+    const value = (easedProgress * target).toFixed(target % 1 === 0 ? 0 : 1);
+
+    el.textContent = value; // обновляем только число
+
+    if (progress < 1) {
+      requestAnimationFrame(updateCounter);
+    }
+  }
+
+  requestAnimationFrame(updateCounter);
+}
+
+const mainPart = document.getElementById("mainPart");
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      document.querySelectorAll(".counter").forEach(counter => {
+        animateCounter(counter, 800);
+      });
+      observer.unobserve(mainPart);
+    }
+  });
+}, { threshold: 0.3 });
+
+observer.observe(mainPart);
